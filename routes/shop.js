3b24726fde87c23e8db3b0a2c3b0e19d51cb5304
mannemraviteja25 <mannemraviteja25@gmail.com>
2024-01-04 02:32:27 +0530

    deleted all to start fresh

--- routes/shop.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const shopController = require('../controllers/shop');
-
-
-router.get('/products',shopController.getProducts);
-router.get('/product/:prodId',shopController.getProduct);
-// router.get('/edit-product/:prodId)
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/',(req,res)=>{
-    res.send("Hii This is Users Page");
-})
-
-module.exports = router;
